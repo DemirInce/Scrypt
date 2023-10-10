a5b9927d68091e0422f43d06b9f3261c9689424f
Demir <demirince@ucsb.edu>
2023-10-09 21:35:04 -0700

    Setup_Continued

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "lib/token.h"
+#include "lib/lexer.h"
+#include "lib/parser.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Hello World!\n"; 
+
+    return 0;
+}
