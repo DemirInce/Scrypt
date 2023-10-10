9c0977a300f2d1ae108a81a85fb7e2cbea4838f3
Demir <demirince@ucsb.edu>
2023-10-09 22:24:09 -0700

    Ready_to_work

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,6 @@ using namespace std;
 int main(){
 
     cout << "Hello World!\n"; 
-    token t(1,1,"69",types::NUMBER);
-    cout << t.value << " " << t.line << " " << t.column << endl;
-
-    // TEST 2
 
     return 0;
 }
